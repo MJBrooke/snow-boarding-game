671444a10691cec52edb19afb2dcc654dc8c316f
Michael Brooke <michaelbrooke93@gmail.com>
2024-09-10 20:49:04 +0200

    Add player rotation controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float torqueAmount = 3f;
+    
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) _rb.AddTorque(torqueAmount);
+        else if (Input.GetKey(KeyCode.RightArrow)) _rb.AddTorque(-torqueAmount);
+    }
+}
