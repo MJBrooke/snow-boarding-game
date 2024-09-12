1a5dec612771ec33d709fb79a614224d4f580a2b
Michael Brooke <michaelbrooke93@gmail.com>
2024-09-12 18:41:02 +0200

    Restart the level on either a Finish or Death event. Add particles for both.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private float reloadDelay = 0.5f;
+    
+    private ParticleSystem _bloodParticles;
+
+    private void Start()
+    {
+        _bloodParticles = GetComponentInChildren<ParticleSystem>();
+    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         // This is saying 'has anything collided with my head?'
-        // In this case, if it's the ground, we debug out.
-        if (other.CompareTag("Ground")) Debug.Log("Bonk!");
+        // In this case, if it's the ground, we restart the level.
+        if (other.CompareTag("Ground"))
+        {
+            _bloodParticles.Play(true);
+            Invoke(nameof(ReloadScene), reloadDelay);
+        }
+    }
+    
+    //Wrap static function from SceneManager into a local function for the invoker
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private float reloadDelay = 2f;
+    
+    // Since the entire pole _is_ the trigger, this line triggers for both
+    // the head and the snowboard going through it. It's saying 'what collided with me?'
+    // and in this case, it's two separate things, both belonging to the Player.
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Since the entire pole _is_ the trigger, this line triggers for both
-        // the head and the snowboard going through it. It's saying 'what collided with me?'
-        // and in this case, it's two separate things, both belonging to the Player.
-        if (other.CompareTag("Player")) Debug.Log("Finished!");
+        if (other.CompareTag("Player")) Invoke(nameof(ReloadScene), reloadDelay);
+    }
+
+    //Wrap static function from SceneManager into a local function for the invoker
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
