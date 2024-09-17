ca6294b44b49fa1a34348031bf535c58e3ad032a
Michael Brooke <michaelbrooke93@gmail.com>
2024-09-17 18:32:53 +0200

    Create a particle system of snow behind the player when touching the ground

---
+++ Assets/Scripts/SnowTrail.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SnowTrail : MonoBehaviour
+{
+    // We want to control a snow particle behind the board
+    //  If we're touching the ground, we should switch on the particles.
+    //  When we leave the ground, we turn them off
+    
+    [SerializeField] private ParticleSystem snowParticles;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) snowParticles.Play();
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        snowParticles.Stop();
+    }
+}
